d8d73b68437ce041d4aecc183a4608f1adcab37e
abrilxcx <291101.owo@gmail.com>
2022-04-07 22:36:17 -0500

    The file has been moved to the inner_join folder
    I don't know why the file saved out of the inner_join folder when i
    saved it.

--- inner_join/inner_join.sql
+++ inner_join/inner_join.sql
@@ -1,18 +1,17 @@
-select 
-
+select -- this part is only for make a view of the table
 Libros.IdLibro, 
-Libros.Nombre as Libros, -- "" as "", sirve para ponerle un alias a un campo para que no se repitan nombres
+Libros.Nombre as Libros, -- "" as "", is used to give an alias to a field to avoid repeating names when the inner table is showing
 Autores.Nombre as Autor, 
 Editoriales.Nombre as Editorial, 
 Categorias.TipoCategoria as Categoria
 
-from Libros -- tabla base
+from Libros -- base table
 
--- [tabla que se va a unir] on [en que parte de la tabla base va] = [que parte de la tabla que se va a unir va ahí]
+-- [2nd table to be joined] on [where in the 1st table it goes] = [what part of the 2nd table is going to join]
 inner join Autores on Libros.IdAutor = Autores.IdAutor 
 inner join Editoriales on Libros.IdEditorial = Editoriales.IdEditorial
 inner join Categorias on Libros.IdCategoria = Categorias.IdCategoria
 
-select * from Libros
+select * from Libros 
 select * from Editoriales
 select * from Categorias
